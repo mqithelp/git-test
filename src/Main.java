f3614a1b484ef58f2c05bf05dba8b112e668ab28
MaxHome <mqithelp@gmail.com>
2025-01-07 22:32:55 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        int k = 1_000_000_000;
+
+        {
+            long sum = 0;
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < k + 1; i++) {
+                sum += i;
+            }
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Время выполнения цикла: " + duration + " мс");
+            System.out.println("Sum=" + sum);
+        }
+
+        {
+            long start = System.currentTimeMillis();
+            long sum = LongStream.rangeClosed(1, k) // Используем LongStream для работы с long
+                    .parallel() // Делаем поток параллельным
+                    .reduce(0L, Long::sum); // Суммируем значения
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Sum=" + sum);
+            System.out.println("Время выполнения параллельного потока: " + duration + " мс");
+        }
+
+        {
+            long start = System.currentTimeMillis();
+            long sum = Stream.iterate(1, a -> a + 1).limit(k).reduce(0L, Long::sum, Long::sum);
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Sum=" + sum);
+            System.out.println("Время выполнения потока: " + duration + " мс");
+        }
+
+    }
+}
